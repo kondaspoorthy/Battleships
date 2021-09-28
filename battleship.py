cb249ee1b3a7572dd43dca0168670df6514adabf
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-09-28 12:15:20 +0530

    updated the implementation of addships

--- battleship.py
+++ battleship.py
@@ -109,8 +109,8 @@ def addShips(grid, numShips):
     num=0
     while(num<numShips):
         ship=createShip()
-        s=checkShip(grid,ship)
-        if(s==True):
+        result=checkShip(grid,ship)
+        if(result==True):
             for i in range(0,3):
                 grid[ship[i][0]][ship[i][1]]=SHIP_UNCLICKED
             num = num + 1
