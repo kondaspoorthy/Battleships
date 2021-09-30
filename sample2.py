7a1d4101460d945003eeadf1f683a16cd8ab12fb
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-09-30 11:36:06 +0530

    completed the implementation of IsHorizontal and IsVertical Functions

---
+++ sample2.py
@@ -0,0 +1,7 @@
+a=int(input("Ener the value of a"))
+b=int(input("Ener the value of b"))
+c=int(input("Ener the value of c"))
+if(a==b==c):
+    print("They are Equal")
+else:
+    print("They  are Unequal")
