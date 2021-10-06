a43e337d50de4ceef45e3c28cfa7f78a8dabcdc6
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-10-06 11:28:48 +0530

    completed the implementation of functions in Restarting the game

--- battleship.py
+++ battleship.py
@@ -358,15 +358,16 @@ Returns: None
 '''
 def drawGameOver(data, canvas):
     fnt=('Times',40,'bold')
+    fnt1=('Times',20,'bold')
     if(data["winner"]=="user"):
         canvas.create_text(250,250,text="congraluations",fill="pink",font=fnt)
-        canvas.create_text(350,300,text="Want to play again press Enter",fill="green",font=fnt)
+        canvas.create_text(300,400,text="Want to try again? press Enter ",fill="green",font=fnt1)
     elif(data["winner"]=="comp"):
         canvas.create_text(250,250,text="Tryagain",fill="red",font=fnt)
-        canvas.create_text(350,300,text="Want to play again press Enter",fill="green",font=fnt)
+        canvas.create_text(300,400,text="Want to try again? press Enter ",fill="green",font=fnt1)
     elif(data["winner"]=="draw"):
         canvas.create_text(250,250,text="draw TryAgain",fill="orange",font=fnt)
-        canvas.create_text(350,300,text="Want to play again press Enter",fill="green",font=fnt)
+        canvas.create_text(300,400,text="Want to try again? press Enter ",fill="green",font=fnt1)
     return
 
 
